501e8643aa528d966954be4976f7757b8f897af4
Paul Schütte <p.schuette@searchmetrics.com>
2020-05-08 07:41:09 +0200

    More clear messaging and option to install locally

--- installers/install.sh
+++ installers/install.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
 # exit if the param is not given
-: ${1?:"Please give the name of the tool to install as argument"}
+USAGE="Please provide the name of the tool to install as argument.
+
+Available tools are:
+ - aws_cli            installs the AWS CLI for current user to
+                       '${HOME}/bin/aws-cli'
+ - loopback_alias     adds the IP address 10.254.254.254 as an alias to
+                      the local network interface to provide a common IP address
+                      for docker containers to reach the host
+
+Example:
+  curl -sq https://raw.githubusercontent.com/searchmetrics/public_gists/master/installers/install.sh | bash -s aws_cli
+"
+if [ $# == 0 ] ; then
+    echo -e "$USAGE"
+    exit 1;
+fi
 
 INSTALL_TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 # Make sure it gets removed even if the script exits abnormally.
@@ -9,8 +24,22 @@ INSTALL_TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 trap "exit 1"                      HUP INT PIPE QUIT TERM
 trap 'rm -rf "${INSTALL_TMP_DIR}"' EXIT
 
+if [ -f ${1}/install.sh ]
+then
+  cp ${1}/install.sh ${INSTALL_TMP_DIR}
+fi
+
 cd ${INSTALL_TMP_DIR}
 
-# download and execute requested script
-curl -sq https://raw.githubusercontent.com/searchmetrics/public_gists/master/installers/${1}/install.sh | bash -s
+# (download and) execute requested script
+if [ ! -f install.sh ]
+then
+  curl -sq https://raw.githubusercontent.com/searchmetrics/public_gists/master/installers/${1}/install.sh -o install.sh
+fi
+if [ "$(head -c 2 install.sh)" = "#!" ]
+then
+  cat install.sh | bash -s
+else
+  echo "No valid installer for ${1} found."
+fi
 
